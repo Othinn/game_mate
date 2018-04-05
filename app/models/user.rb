58b26f180810ae7366bd2e2083f4c71cb02b8474
Othinn <kurak1993@gmail.com>
2018-04-05 20:14:08 +0200

    added registration and login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(['lower(username) = :value OR lower(email) = :value', {value: login.strip.downcase}]).first
+  end
 end
